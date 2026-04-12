fa5a3fde879a58b1cee05e83c436a7151aeb2aaf
Manav Kamdar <mjkamdar04@gmail.com>
2026-04-12 12:16:38 -0400

    Setup

---
+++ nestjs-microservices/apps/catalog/src/main.ts
@@ -0,0 +1,34 @@
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { CatalogModule } from "./catalog.module";
+
+async function bootstrap(){
+  process.title = 'catalog'
+
+  const logger = new Logger('CatalogBootstrap')
+  const port = Number(process.env.CATALOG_TCP_PORT ?? 4011)
+
+  //Create a microservice instance
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CatalogModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port,
+      }
+    }
+  )
+
+  app.enableShutdownHooks();
+
+
+  await app.listen()
+
+  logger.log(`Catalog microservice (TCP) listening on port ${port}`)
+}
+
+
+bootstrap()
